658ebed3a7876a004584f49a1485a0ed05423b74
Yash Digarse <112575988+yashdigarse@users.noreply.github.com>
2024-11-19 23:20:18 -0500

    rapid

--- main.py
+++ main.py
@@ -4,29 +4,24 @@ import requests
 import io
 # Define the function to apply the cartoon effect
 def apply_cartoon_effect(image_data, api_key):
-    url = 'https://api.lightxeditor.com/external/api/v1/cartoon'
+    url = 'https://api.rapidapi.com/cartoon-generator'
     headers = {
-        'x-api-key': api_key,  # Replace with your actual API key
-        'Content-Type': 'application/json'
+        'x-rapidapi-key': api_key,  # Replace with your actual API key
+        'x-rapidapi-host': 'api.rapidapi.com',
+        'Content-Type': 'application/octet-stream'
     }
-    files = {'image': image_data}
-    data = {
-        "styleImageUrl": "https://ai.flux-image.com/flux/d4533a74-9d35-4683-8a05-18c0b646ad36.jpg",  # Replace with the URL of your input style image
-        "textPrompt": "Cartoon style with vibrant colors and exaggerated features"  # Replace with your specific input prompt
-    }
-    response = requests.post(url, headers=headers, files=files, data=data)
+    response = requests.post(url, headers=headers, data=image_data)
     return response
 
 # Streamlit app layout
-st.title("Cartoon Effect App")
+st.title("AI Cartoon Generator")
 
 # Input fields
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-api_key = "e456a84c544e4576bd71888f4e8f7d98_fd59cad6cf8848e98b2b28abf80b12f4_andoraitools"
+api_key = "58c8cb483emshb1f84eba4a5f694p1ab7cbjsn87bda417be10"
 
 # Button to trigger the cartoon effect
-if st.button("Apply Cartoon Effect"):
+if st.button("Generate Cartoon"):
     if uploaded_file is not None:
         image_data = uploaded_file.read()
         response = apply_cartoon_effect(image_data, api_key)
@@ -35,7 +30,7 @@ if st.button("Apply Cartoon Effect"):
         if response.status_code == 200:
             st.success("Request was successful!")
             response_json = response.json()
-            output_url = response_json.get("body", {}).get("output")
+            output_url = response_json.get("output")
             if output_url:
                 st.image(output_url, caption="Cartoon Effect Image")
             else:
